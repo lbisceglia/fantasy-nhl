39f9f1c0aae19c4c7fd6f28f4c7032dc835ef58a
Lorenzo Bisceglia <lorenzo.bisceglia9@gmail.com>
2018-11-22 23:32:55 -0800

    Created Add Team scene in JavaFX.

--- src/ui/GUI.java
+++ src/ui/GUI.java
@@ -1,18 +1,26 @@
 package ui;
 
+import exceptions.InvalidFantasyWeekException;
+import exceptions.InvalidTeamException;
 import interfaces.Loadable;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import managers.FantasyManager;
 import models.League;
+import models.Team;
 
 import java.io.*;
+
+import static models.League.MAX_PARTICIPANTS;
+import static models.League.MIN_PARTICIPANTS;
 
 public class GUI extends Application implements Loadable, Serializable {
     private final static int WIDTH = 500;
@@ -21,25 +29,32 @@ public class GUI extends Application implements Loadable, Serializable {
     private final static Font btnFont = new Font(FONT, 16);
 
     private FantasyManager fantasyManager;
-    private Label lblLoadComplete = new Label();
+    private Label instructions = new Label();
+    private TextField inputTeam;
     private Stage window;
     private Scene startPage;
     private Scene mainMenu;
     private Scene addTeamMenu;
     private Scene deleteTeamMenu;
-    private Scene viewTeams;
-    private Scene leaderboard;
-
+    private Scene Standings;
 
     private Button btnGetStarted;
     private Button btnAddTeam;
     private Button btnDeleteTeam;
-    private Button btnViewTeams;
+    private Button btnViewStandings;
+    private Button btnDraft;
+    private Button btnAdvanceWeek;
+    private Button btnSave;
+    private Button btnQuit;
+    private Button btnBack;
+    private Button btnSubmitAddTeam;
 
     public static void main(String[] args) {
         launch(args);
     }
 
+    // TODO: Make sure correct buttons are faded even after loading from a saved state
+    // Something like a *check button state* method run in the top of the constructor might be a good idea
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -51,6 +66,8 @@ public class GUI extends Application implements Loadable, Serializable {
         window.setScene(startPage);
 
         setupMainMenu();
+        setupBackButton();
+        setupAddTeamMenu();
 
         window.show();
     }
@@ -77,41 +94,168 @@ public class GUI extends Application implements Loadable, Serializable {
         btnAddTeam = new Button();
         btnAddTeam.setText("Add Team");
         btnAddTeam.setFont(btnFont);
+        btnAddTeam.setDisable(!teamCanBeAdded());
         btnAddTeam.setOnAction(e -> {
-            //TODO: add functionality
+            window.setScene(addTeamMenu);
         });
 
         btnDeleteTeam = new Button();
         btnDeleteTeam.setText("Delete Team");
         btnDeleteTeam.setFont(btnFont);
-        btnDeleteTeam.setDisable(true);
+        // TODO: change from true/false to dependent on those boolean fields
+        btnDeleteTeam.setDisable(!teamCanBeRemoved());
         btnDeleteTeam.setOnAction(e -> {
 
         });
 
+        btnViewStandings = new Button();
+        btnViewStandings.setText("View Teams");
+        btnViewStandings.setFont(btnFont);
+        btnViewStandings.setOnAction(e -> {
+
+        });
+
+        btnDraft = new Button();
+        btnDraft.setText("Draft");
+        btnDraft.setFont(btnFont);
+        btnDraft.setDisable(!leagueCanDraft());
+        btnDraft.setOnAction(e -> {
+
+        });
+
+        btnAdvanceWeek = new Button();
+        btnAdvanceWeek.setText("Advance Week");
+        btnAdvanceWeek.setFont(btnFont);
+        // TODO: change from true/false to dependent on those boolean fields
+        btnAdvanceWeek.setDisable(!(leagueIsDrafted() && leagueCanAdvance()));
+        btnAdvanceWeek.setOnAction(e -> {
+
+        });
+
+        btnSave = new Button();
+        btnSave.setText("Save & Quit");
+        btnSave.setFont(btnFont);
+        btnSave.setOnAction(e -> {
+
+        });
+
+        btnQuit = new Button();
+        btnQuit.setText("Quit");
+        btnQuit.setFont(btnFont);
+        btnQuit.setOnAction(e -> {
+
+        });
+
+
         VBox mainMenuLayout = new VBox(10);
         mainMenuLayout.setAlignment(Pos.CENTER);
-        mainMenuLayout.getChildren().addAll(btnAddTeam, btnDeleteTeam);
+        mainMenuLayout.getChildren().addAll(btnAddTeam, btnDeleteTeam, btnViewStandings, btnDraft, btnAdvanceWeek, btnSave, btnQuit);
 
         mainMenu = new Scene(mainMenuLayout, WIDTH, HEIGHT);
     }
 
-//    private void setupLoadingPage() {
-//        Label lblLoading = new Label("Loading the league!" + "\n" + "This may take a moment...");
-//        lblLoading.setFont(new Font("Arial", 20));
-//        Label lblLoadComplete = new Label("");
-//        btnEnterApp = new Button();
-//        btnEnterApp.setText("Enter");
-//        btnEnterApp.setDisable(true);
-//        btnEnterApp.setOnAction(e -> window.setScene(mainMenu));
-//
-//        VBox loadingLayout = new VBox(20);
-//        loadingLayout.setAlignment(Pos.CENTER);
-//        loadingLayout.getChildren().add(lblLoading);
-//        loadingLayout.getChildren().add(lblLoadComplete);
-//
-//        loadingPage = new Scene(loadingLayout, WIDTH, HEIGHT);
-//    }
+    private void setupBackButton() {
+        btnBack = new Button();
+        btnBack.setText("Back");
+        btnBack.setFont(btnFont);
+        btnBack.setOnAction(e-> {
+            updateAddTeamInstructions();
+            window.setScene(mainMenu);
+        });
+    }
+
+    private void setupAddTeamMenu() {
+        btnSubmitAddTeam = new Button();
+        btnSubmitAddTeam.setText("Create Team");
+        btnSubmitAddTeam.setFont(btnFont);
+        btnSubmitAddTeam.setOnAction(e -> {
+            createUserGeneratedTeam();
+        });
+        instructions = new Label();
+
+        inputTeam = new TextField();
+
+        VBox addTeamLayout = new VBox(10);
+        addTeamLayout.setAlignment(Pos.CENTER);
+        addTeamLayout.getChildren().addAll(instructions, inputTeam, btnSubmitAddTeam, btnBack);
+
+        addTeamMenu = new Scene(addTeamLayout, WIDTH, HEIGHT);
+    }
+
+    private void updateAddTeamInstructions() {
+        if(atLeastOneTeam()) {
+            instructions.setText(printActiveTeamNames());
+        } else {
+            instructions.setText("You're the first team in the league!" + "\n" +
+                    "Please enter a unique team name.");
+        }
+    }
+
+    private boolean atLeastOneTeam() {
+        return fantasyManager.size() > 0;
+    }
+
+    private boolean teamCanBeAdded() {
+        return fantasyManager.size() < MAX_PARTICIPANTS;
+    }
+
+    private boolean teamCanBeRemoved() {
+        return (atLeastOneTeam() && !leagueIsDrafted());
+    }
+
+    private boolean leagueCanDraft() {
+        return (fantasyManager.size() >= MIN_PARTICIPANTS && !fantasyManager.isDrafted());
+    }
+
+    private boolean leagueIsDrafted() {
+        return fantasyManager.isDrafted();
+    }
+
+    private boolean leagueCanAdvance() {
+        try {
+            fantasyManager.checkFantasyWeekHasFullyElapsed(fantasyManager.currentWeek());
+            return true;
+        } catch (InvalidFantasyWeekException e) {
+            return false;
+        }
+    }
+
+    private void createUserGeneratedTeam() {
+        String teamName = String.valueOf(inputTeam.getText());
+
+        try {
+            Team team = new Team(teamName.toString());
+            fantasyManager.getLeague().addTeam(team);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Dialog");
+            alert.setContentText("Success! " + teamName + " was added to the fantasy league.");
+            alert.showAndWait();
+            inputTeam.clear();
+            window.setScene(mainMenu);
+        } catch (InvalidTeamException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalid Team Error");
+            alert.setContentText(e.getMsg());
+            alert.showAndWait();
+        }
+    }
+
+
+    private String printActiveTeamNames() {
+        String teams = "Here is the list of teams: ";
+        if (atLeastOneTeam()) {
+            int i = 1;
+            for (Team t : fantasyManager.getLeague().getTeams()) {
+                String title = "\n" + "[" + i + "] - " + t.getTeamName();
+                teams += title;
+                i++;
+            }
+        } else {
+            System.out.println("You're the first team in the league!");
+        }
+        return teams;
+    }
+
 
     @Override
     //Modeled after Object Stream tutorial, 2018-10-01 [https://www.mkyong.com/java/how-to-read-and-write-java-object-to-a-file/]
